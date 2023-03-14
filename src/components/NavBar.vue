c819c72727c1e1636c3d2cc1cbfe93822f1113ba
Clebson Morais <clebson.cont@gmail.com>
2023-03-14 16:58:14 -0300

    Deletando os dados padrões da aplicação vue e começando a fazeros componentes necessários para a aplicação

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+        <img :src="logo" id="logo" :alt="alt">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'NavBar',
+    props: ["logo", "alt"]
+}
+
+</script>
+
+<style scoped>
+
+</style>
